af522dc9cf2a4dd12f5cb8573eb92f0ae9e9c8f2
Sandrava Philips <lacasandra1@gmail.com>
2020-05-13 16:38:05 +0100

    built the database model

---
+++ dbModel.js
@@ -0,0 +1,43 @@
+const db = require('./dbConfig');
+
+module.exports = {
+    addUser: function(user) {
+        return db('users')
+            .insert(user)
+            .returning('id')
+            .then(([id]) => {
+                return this.findUserBy({ id });
+            });
+    },
+    findUserBy: function(filter) {
+        return db('users')
+            .where(filter)
+            .first()
+    },
+    addPicture: function(picture) {
+        return db('gallery')
+            .insert(picture)
+            .returning('id')
+            .then(([id]) => {
+                return this.findPictureBy({ id });
+            });
+    },
+    findPictureBy: function(filter) {
+        return db('gallery')
+            .where(filter)
+            .first()
+    },
+    addDiaryText: function(text) {
+        return db('diary')
+            .insert(text)
+            .returning('id')
+            .then(([id]) => {
+                return this.findTextBy({ id });
+            });
+    },
+    findTextBy: function(filter) {
+        return db('diary')
+            .where(filter)
+            .first()
+    },
+}
