e58ec4b1ec61d3a274806dd27b8e692f72062c6e
Sandrava Philips <lacasandra1@gmail.com>
2020-05-15 20:59:52 +0100

    included delete and put methods

--- router/userRouter.js
+++ router/userRouter.js
@@ -23,16 +23,6 @@ router.post('/gallery', validateBody, async(req, res) => {
     }
 })
 
-router.post('/gallery', validateBody, async(req, res) => {
-    try {
-        const newPicture = await db.addPicture({ ...req.body, userId: req.decodedToken.subject });
-        res.status(201).json({ message: variables.newEntry, newPicture })
-    }
-    catch (error) {
-        res.status(500).json({ message: variables.errorMessage, error: error.message })
-    }
-})
-
 router.get('/gallery', async(req, res) => {
     try {
         const picture = await db.findPictureBy({userId: req.decodedToken.subject });
@@ -53,3 +43,27 @@ router.get('/diary/:id', async(req, res) => {
         res.status(500).json({ message: variables.errorMessage, error: error.message })
     }
 })
+
+router.put('/diary/:id', validateBody, async(req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedDiary = await db.updateDiaryText(id, req.body);
+        res.status(200).json({ message: variables.updatedEntry, updatedDiary });
+    }
+    catch (error) {
+        res.status(500).json({ message: variables.errorMessage, error: error.message })
+    }
+})
+
+router.delete('/gallery/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        await db.removePicture(id);
+        res.status(200).json({ message: variables.pictureRemoved('Picture') });
+    }
+    catch (error) {
+        res.status(500).json({ message: variables.errorMessage, error: error.message })
+    }
+})
+
+module.exports = router;
