cf3435d34c85619f66ba0fa4fadc58307cecc556
Sandrava Philips <lacasandra1@gmail.com>
2020-05-15 20:57:49 +0100

    corrected an error source

--- data/migrations/20200513154656_diary.js
+++ data/migrations/20200513154656_diary.js
@@ -2,7 +2,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('diary', table => {
         table.increments();
-        table.text('diaryText', longtext);
+        table.text('diaryText', 'longtext');
         table.date('date').unique().notNullable();
         table.integer('userId').unsigned().notNullable().references('id').inTable('users').onUpdate('CASCADE').onDelete('CASCADE');
     });
