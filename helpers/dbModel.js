c18f52ea62e66d2e8381ef53b2250df1c83ceb0c
Sandrava Philips <lacasandra1@gmail.com>
2020-05-15 19:48:02 +0100

    made corrections to the findPictureBy function

--- helpers/dbModel.js
+++ helpers/dbModel.js
@@ -25,7 +25,6 @@ module.exports = {
     findPictureBy: function(filter) {
         return db('gallery')
             .where(filter)
-            .first()
     },
     addDiaryText: function(text) {
         return db('diary')
