0f80f80d5125734cf6883d1905e560495665fc19
Sandrava Philips <lacasandra1@gmail.com>
2020-05-15 21:01:08 +0100

    imported the router and made corresponding use cases

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const server = express();
 
+const authRouter = require('./router/authRouter');
+const userRouter = require('./router/userRouter');
+const { validateBody, validateEmail, validateUser, restricted } = require('./helpers/middleware');
+
+server.use(express.json());
+server.use(helmet());
+server.use(cors());
+
+server.use('/api/auth', validateBody, validateEmail, validateUser, authRouter);
+server.use('/api', restricted, userRouter);
+
 server.get('/', (req, res) => {
-  res.send('Hello from Express');
+  res.send('Hello from Dear Diary');
 });
 
 server.listen(5000, () =>
