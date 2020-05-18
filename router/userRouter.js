bb6e56d7e8babdbdde06641ed41ca2c1a1cd622e
Sandrava Philips <lacasandra1@gmail.com>
2020-05-18 16:56:53 +0100

    modified the post picture router

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const fileUpload = require('express-fileupload');
 const db = require('../helpers/dbModel');
 const variables = require('../helpers/variables');
-const { validateBody } = require('../helpers/middleware');
+const { validateBody, validatePicture } = require('../helpers/middleware');
+const cloudinary = require('../config/clConfig');
+
+router.use(fileUpload({
+    useTempFiles: true
+}))
+
 
 router.post('/diary', validateBody, async(req, res) => {
     try {
@@ -13,20 +20,24 @@ router.post('/diary', validateBody, async(req, res) => {
     }
 })
 
-router.post('/gallery', validateBody, async(req, res) => {
+router.post('/gallery', validatePicture, async(req, res) => {
     try {
-        const newPicture = await db.addPicture({ ...req.body, userId: req.decodedToken.subject });
-        res.status(201).json({ message: variables.newEntry, newPicture })
+        const file = req.files.photo;
+        cloudinary.uploader.upload(file.tempFilePath, async (err, result) => {
+            const newPicture = await db.addPicture({ date: result.created_at.substring(0, 10), picture: result.url, userId: req.decodedToken.subject });
+            res.status(201).json({ message: variables.newEntry, newPicture })
+        })        
     }
     catch (error) {
         res.status(500).json({ message: variables.errorMessage, error: error.message })
     }
 })
 
-router.get('/gallery', async(req, res) => {
+router.get('/gallery/:date', async(req, res) => {
     try {
-        const picture = await db.findPictureBy({userId: req.decodedToken.subject });
-        res.status(201).json({ picture })
+        const date = req.params.date;
+        const pictures = await db.findPictureBy({userId: req.decodedToken.subject, date });
+        res.status(201).json({ pictures })
     }
     catch (error) {
         res.status(500).json({ message: variables.errorMessage, error: error.message })
