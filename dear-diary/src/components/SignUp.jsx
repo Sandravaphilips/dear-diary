f9c53afb573052591c950a050da211d4baa83b93
Sandrava Philips <lacasandra1@gmail.com>
2020-05-26 19:31:29 +0100

    added the needed components

---
+++ dear-diary/src/components/SignUp.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { TextField, Button } from '@material-ui/core';
+
+const SignUp = props => {
+    const onHandleSubmit = e => {
+        e.preventDefault()
+        if(e.target.password !== e.target.confirmPassword) {
+            alert('Passwords must match!!')
+        } else {
+            const payload = {
+                username: e.target.username,
+                emailAddress: e.target.email,
+                password: e.target.password
+            }
+            axios.post('https://my-dear-diary.herokuapp.com/api/auth/register', payload)
+            .then(response => {
+                localStorage.setItem("token", response.data.token);            
+                props.history.push('/diary');
+            })
+            .catch(error => {
+                alert(error);
+            });
+        }        
+    }
+
+    return(
+        <div>
+            <form>
+                <TextField required id="username" label="Username" defaultValue="JohnSmith" />
+                <TextField required id="email" label="Email Address" defaultValue="johnsmith@gmail.com" />                
+                <TextField
+                    required
+                    id="password"
+                    label="Password"
+                    type="password"
+                    defaultValue='#2password@'
+                /> 
+                <TextField
+                    required
+                    id="confirmPassword"
+                    label="Confirm Password"
+                    type="password"
+                    defaultValue='#2password@'
+                />
+                <Button onClick={onHandleSubmit} variant="contained" color="primary">
+                    Submit
+                </Button>
+
+                <p>Already have an account? <Link to='/login'>Log In</Link></p> 
+            </form>
+        </div>
+    )
+};
+
+export default SignUp
