17291dc9bb65d1db9e37153a1d2d4cc1ffd72598
Sandrava Philips <lacasandra1@gmail.com>
2020-06-24 17:02:23 +0100

    implemented react router

--- dear-diary/src/App.js
+++ dear-diary/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Route } from "react-router-dom";
 import './App.css';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Dashboard from './components/Dashboard';
+import Diary from './components/Diary';
 
 function App() {
   return (
     <div className="App">
-      
+      <Route exact path='/' component={Login} />
+      <Route exact path='/signup' component={SignUp} />
+      <Route exact path='/dashboard' component={Dashboard} />
+      <Route exact path='/diary' component={Diary} />
     </div>
   );
 }
