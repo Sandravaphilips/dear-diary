d0caabb35a9050dcfb6fba6f66ba794e7f9fce65
Sandrava Philips <lacasandra1@gmail.com>
2020-06-27 15:28:00 +0100

    included a router for DiaryCalendar

--- dear-diary/src/App.js
+++ dear-diary/src/App.js
@@ -5,6 +5,7 @@ import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Dashboard from './components/Dashboard';
 import Diary from './components/Diary';
+import DiaryCalendar from './components/DiaryCalendar';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <Route exact path='/' component={Login} />
       <Route exact path='/signup' component={SignUp} />
       <Route exact path='/dashboard' component={Dashboard} />
-      <Route exact path='/diary' component={Diary} />
+      <Route exact path='/diary/:date' component={Diary} />
+      <Route exact path='/calendar' component={DiaryCalendar} />
     </div>
   );
 }
