87da3349301a378e3507bfe2c656062af62e56aa
Sandrava Philips <lacasandra1@gmail.com>
2020-06-27 15:29:43 +0100

    SignUp works

--- dear-diary/src/components/SignUp.jsx
+++ dear-diary/src/components/SignUp.jsx
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { TextField, Button } from '@material-ui/core';
 
+const initialFormValues = {
+    emailAddress: '',
+    password: '',
+    username: ''
+};
+
 const SignUp = props => {
+    const [formValues, setFormValues] = useState(initialFormValues);
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const onEmailChange = e => {
+        setFormValues({...formValues, emailAddress: e.target.value});
+    }
+
+    const onPasswordChange = e => {
+        setFormValues({...formValues, password: e.target.value});
+    }
+
+    const onUsernameChange = e => {
+        setFormValues({...formValues, username: e.target.value});
+    }
+
+    const onConfirmPasswordChange = e => {
+        setConfirmPassword(e.target.value);
+    }
+
     const onHandleSubmit = e => {
         e.preventDefault()
-        if(e.target.password !== e.target.confirmPassword) {
+        if(formValues.password !== confirmPassword) {
             alert('Passwords must match!!')
         } else {
-            const payload = {
-                username: e.target.username,
-                emailAddress: e.target.email,
-                password: e.target.password
-            }
-            axios.post('https://my-dear-diary.herokuapp.com/api/auth/register', payload)
+            axios.post('https://my-dear-diary.herokuapp.com/api/auth/register', formValues)
             .then(response => {
                 localStorage.setItem("token", response.data.token);            
-                props.history.push('/login');
+                props.history.push('/dashboard');
             })
             .catch(error => {
                 alert(error);
@@ -28,21 +48,35 @@ const SignUp = props => {
     return(
         <div>
             <form>
-                <TextField required id="username" label="Username" defaultValue="JohnSmith" />
-                <TextField required id="email" label="Email Address" defaultValue="johnsmith@gmail.com" />                
                 <TextField
+                    onChange={onUsernameChange} 
+                    required 
+                    id="username" 
+                    label="Username" 
+                    placeholder="JohnSmith" 
+                />
+                <TextField 
+                    onChange={onEmailChange}
+                    required 
+                    id="email" 
+                    label="Email Address" 
+                    placeholder="johnsmith@gmail.com" 
+                />                
+                <TextField
+                    onChange={onPasswordChange}
                     required
                     id="password"
                     label="Password"
                     type="password"
-                    defaultValue='#2password@'
+                    placeholder='#2password@'
                 /> 
                 <TextField
+                    onChange={onConfirmPasswordChange}
                     required
                     id="confirmPassword"
                     label="Confirm Password"
                     type="password"
-                    defaultValue='#2password@'
+                    placeholder='#2password@'
                 />
                 <Button onClick={onHandleSubmit} variant="contained" color="primary">
                     Submit
