cb0173a2fb89ad9bf2a549d064b7aff4d70ecdb6
Sandrava Philips <lacasandra1@gmail.com>
2020-07-02 13:35:01 +0100

    removed the second cors function

--- server.js
+++ server.js
@@ -13,12 +13,6 @@ server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 server.use('/api/auth', validateBody, validateEmail, validateUser, authRouter);
 server.use('/api', restricted, userRouter);
 
