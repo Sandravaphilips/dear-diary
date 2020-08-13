a02dcfdb8f2f6257d1cd8f1ccc49ca8bb3732b0d
Sandravaphilips <lacasandra1@gmail.com>
2020-08-13 01:06:21 -0700

    further built Navigation

--- dear-diary/src/components/Navigation..jsx
+++ dear-diary/src/components/Navigation..jsx
@@ -1,58 +1,56 @@
-import React from 'react';
-import logo from './assets/logo.svg';
-import { AppBar, Toolbar, IconButton, Button, MenuItem, Menu, Fade } from '@material-ui/core';
-import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+import React, { useState, useRef } from 'react';
+import { Link } from "react-router-dom";
+
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
+import { MenuRounded, CloseRounded } from '@material-ui/icons';
+
 import { NavigationStyle, MenuStyle } from './style';
+import useOnClickOutside from './hooks/useOnClickOutside';
+import logo from './assets/logo.svg';
+
 
 const Navigation = () => {
-    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [open, setOpen] = useState(false);
+    const node = useRef(); 
+    useOnClickOutside(node, () => setOpen(false));
 
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
+    const handleClick = () => {
+        setOpen(!open);
     };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+    const onLogout = (e) => {
+        localStorage.removeItem("token");
+    }
+
     return(
-        <NavigationStyle>
+        <NavigationStyle ref={node}>
             <AppBar position="static">
                 <Toolbar>
                     <IconButton edge="start" className='menu-button' color="inherit" aria-label="menu" onClick={handleClick}>
-                    <MenuRoundedIcon />
+                    <MenuRounded />
                     </IconButton>
                     <figure>
                         <img src={logo} alt='app-logo' />
                     </figure>
-                    <Button color="inherit">Logout</Button>
+                    <Link to='/' className='logout' onClick={onLogout}>Logout</Link>
                 </Toolbar>
-            </AppBar>
-            <Menu
-                id="fade-menu"
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}                
-                TransitionComponent={Fade}
-            >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-                <MenuItem onClick={handleClose}>Logout</MenuItem>
-            </Menu>
+            </AppBar>            
+            <Menu open={open} handleClick={handleClick} onLogout={onLogout} />
         </NavigationStyle>
     )
 };
 
-// const Menu = () => {
-//     return(
-//         <MenuStyle>
-//             <ul>
-//                 <li>Dashboard</li>
-//                 <li>Calendar</li>
-//                 <li>Logout</li>
-//             </ul>
-//         </MenuStyle>
-//     )
-// }
+const Menu = ({open, handleClick, onLogout}) => {
+    return(
+        <MenuStyle open={open}>
+            <CloseRounded onClick={handleClick} className='close-button' />
+            <ul>
+                <li><Link to='/dashboard'>Dashboard</Link></li>
+                <li><Link to='/calendar'>Calendar</Link></li>
+                <li onClick={onLogout}><Link to='/'>Logout</Link></li>
+            </ul>
+        </MenuStyle>
+    )
+}
 
 export default Navigation
