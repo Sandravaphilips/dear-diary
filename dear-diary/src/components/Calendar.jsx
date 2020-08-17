c1d0af04183fdd8440e9b90f766c382ea2b4ff07
Sandravaphilips <lacasandra1@gmail.com>
2020-08-17 00:56:31 -0700

    built Calendar

---
+++ dear-diary/src/components/Calendar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import DiaryCalendar from './DiaryCalendar';
+import Navigation from './Navigation.';
+import { CalendarStyle } from './style';
+
+const Calendar = props => {
+    return(
+        <CalendarStyle>
+            <Navigation />
+            <p>Note To Self: Click on day to view<span>😊</span></p>
+            <DiaryCalendar {...props} />
+            <footer>
+                <p>&copy; Dear Diary</p>
+            </footer>
+        </CalendarStyle>
+    )
+}
+
+export default Calendar
