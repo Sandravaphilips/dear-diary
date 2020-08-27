b4ecdd6b8163f455b7c23d28f62d32d13a23a0d6
Sandrava Philips <lacasandra1@gmail.com>
2020-08-27 16:58:28 +0100

    refactored the clodinary code

--- router/userRouter.js
+++ router/userRouter.js
@@ -23,10 +23,34 @@ router.post('/diary', validateBody, async(req, res) => {
 router.post('/gallery', validatePicture, async(req, res) => {
     try {
         const file = req.files.photo;
-        cloudinary.uploader.upload(file.tempFilePath, async (err, result) => {
-            const newPicture = await db.addPicture({ date: result.created_at.substring(0, 10), picture: result.url, userId: req.decodedToken.subject });
-            res.status(201).json({ message: variables.newEntry, newPicture })
-        }, {upload_preset: 'dear_diary'})        
+        const image = await new Promise(async resolve => {
+            cloudinary.uploader.upload(file.tempFilePath, {upload_preset: 'dear_diary'}, (err, result) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    resolve(
+                        {
+                            url:result.url,
+                            id: result.public_id,
+                            date: result.created_at
+                        },
+                        {
+                            resource_type: 'auto',
+                            folder: 'Images'
+                        }
+                    )
+                }
+            })
+        })
+        // let image;
+
+        // cloudinary.uploader.upload(file.tempFilePath, {upload_preset: 'dear_diary'}, async (err, result) => {
+        //     const newPicture = await db.addPicture({ date: result.created_at.substring(0, 10), picture: result.url, userId: req.decodedToken.subject });
+        //     res.status(201).json({ message: variables.newEntry, newPicture })
+        //     // image = result;
+        // })
+        const newPicture = await db.addPicture({ date: image.date.substring(0, 10), picture: image.url, userId: req.decodedToken.subject });
+        res.status(201).json({ message: variables.newEntry, newPicture })        
     }
     catch (error) {
         res.status(500).json({ message: variables.errorMessage, error: error.message })
