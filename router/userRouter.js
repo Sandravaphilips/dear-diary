c7c9bb3242d0f8f37a9a43de1636d77aa0fd139c
Sandravaphilips <lacasandra1@gmail.com>
2020-10-28 08:00:29 -0700

    removed multer

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-// const fileUpload = require('express-fileupload');
+const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const db = require('../helpers/dbModel');
 const variables = require('../helpers/variables');
@@ -7,9 +7,9 @@ const { validateBody, validatePicture } = require('../helpers/middleware');
 const cloudinaryUploader = require('../config/clConfig');
 const upload = require('../config/multer');
 
-// router.use(fileUpload({
-//     useTempFiles: false
-// }))
+router.use(fileUpload({
+    useTempFiles: true
+}))
 
 function deleteTempFile(tempPath) {
 	fs.unlink(tempPath, function(err) {
@@ -27,44 +27,18 @@ router.post('/diary', validateBody, async(req, res) => {
     }
 })
 
-router.post('/gallery', upload.array('photo'), async(req, res) => {
+router.post('/gallery', validatePicture, async(req, res) => {
     try {
-        const tempPath = req.files[0].path;
-        console.log(req.files)
-        // const file = req.files.photo;
-        const image = await cloudinaryUploader(tempPath, "Images");
+        // const tempPath = req.files[0].path;
+        // console.log(req.files)
+        const file = req.files.photo;
+        // console.log(req.files)
+        const image = await cloudinaryUploader(file.tempFilePath, "Images");
         const newPicture = await db.addPicture({date: image.date.substring(0, 10), picture: image.url, userId: req.decodedToken.subject })
-        deleteTempFile(tempPath)
+        // deleteTempFile(tempPath)
         // console.log(tempPath)
         res.status(201).json({ message: variables.newEntry, newPicture })
-        // const image = await new Promise(async resolve => {
-        //     cloudinary.uploader.upload(file.tempFilePath, {upload_preset: 'dear_diary'}, (err, result) => {
-        //         if (err) {
-        //             console.log(err)
-        //         } else {
-        //             resolve(
-        //                 {
-        //                     url:result.url,
-        //                     id: result.public_id,
-        //                     date: result.created_at
-        //                 },
-        //                 {
-        //                     resource_type: 'auto',
-        //                     folder: 'Images'
-        //                 }
-        //             )
-        //         }
-        //     })
-        // })
-        // let image;
-
-        // cloudinary.uploader.upload(file.tempFilePath, {upload_preset: 'dear_diary'}, async (err, result) => {
-        //     const newPicture = await db.addPicture({ date: result.created_at.substring(0, 10), picture: result.url, userId: req.decodedToken.subject });
-        //     res.status(201).json({ message: variables.newEntry, newPicture })
-        //     // image = result;
-        // })
-        // const newPicture = await db.addPicture({ date: image.date.substring(0, 10), picture: image.url, userId: req.decodedToken.subject });
-        // res.status(201).json({ message: variables.newEntry, newPicture })        
+                
     }
     catch (error) {
         res.status(500).json({ message: variables.errorMessage, error: error.message })
