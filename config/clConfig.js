c4f52a031754778475c097813613868a890e9ddd
Sandravaphilips <lacasandra1@gmail.com>
2020-10-29 05:39:07 -0700

    set cloudinary config to variables

--- config/clConfig.js
+++ config/clConfig.js
@@ -1,9 +1,12 @@
-const cloudinary =  require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2;
+const dotenv = require('dotenv');
+
+dotenv.config()
 
 cloudinary.config({
-    cloud_name: 'hmvhobezm',
-    api_key: '783757666958785',
-    api_secret: 'gqyAAprCpK_c5aFlatRMx-omi5M'
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
 })
 
 const cloudinaryUploader = (file, folder) => {
